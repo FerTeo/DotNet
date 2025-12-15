06d2192254d0a476196a98db14136d1d5a02cbe6
Fernando-Emanuel Donea <fernandodonea10@gmail.com>
2025-12-16 00:48:10 +0200

    update ApplicationUser.cs and gitignore

--- OSSocial/Models/ApplicationUser.cs
+++ OSSocial/Models/ApplicationUser.cs
@@ -10,9 +10,7 @@ namespace OSSocial.Models
     {
         
         // atribute suplimentare adaugate pentru user
-        public string? FirstName { get; set; }
-
-        public string? LastName { get; set; }
+        public string? DisplayName { get; set; }
         
         public string? Bio { get; set; }
         
